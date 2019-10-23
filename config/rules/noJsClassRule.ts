ee1b6f1d45cb3fb7de43f7ab781867c476ea33af
Zhicheng WANG <zcwang@thoughtworks.com>
2019-10-23 11:12:05 +0800

    refactor: 抽取公共函数

--- config/rules/noJsClassRule.ts
+++ config/rules/noJsClassRule.ts
@@ -5,8 +5,14 @@ export class Rule extends Lint.Rules.AbstractRule {
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
     return this.applyWithFunction(sourceFile, walk);
   }
+}
 
-  public static FAILURE_STRING = 'No ES5 class';
+function generateClass(node: ts.FunctionDeclaration | ts.FunctionExpression): string {
+  return node.getText();
+}
+
+function isClassName(name?: ts.Identifier | ts.BindingName): boolean {
+  return !!name && /^(mx)?[A-Z]\w+$/.test(name.getText());
 }
 
 function walk(ctx: Lint.WalkContext): void {
@@ -16,13 +22,29 @@ function walk(ctx: Lint.WalkContext): void {
     // 一旦碰到符合特征的 .xxx = function 表达式，则将其添加到相应的 es6 类中作为静态方法
     // 找到所有对未声明的 this.xxx 属性的赋值，则将其作为属性添加到相应的 es6 类中
     switch (node.kind) {
+      case ts.SyntaxKind.VariableStatement:
+        const n = node as ts.VariableStatement;
+        const decl = n.declarationList.declarations[0];
+        if (decl && decl.initializer && decl.initializer.kind === ts.SyntaxKind.FunctionExpression) {
+          if (isClassName(decl.name)) {
+            const fn = decl.initializer as ts.FunctionExpression;
+            const fix = [
+              new Lint.Replacement(node.getStart(), node.getEnd() - node.getStart(), generateClass(fn).replace(/^function\b ?(\w*)/, `function ${decl.name.getText()}`)),
+            ];
+            ctx.addFailureAtNode(node, `ES5 class - ${decl.name.getText()}`, fix);
+          }
+        }
+        break;
       case ts.SyntaxKind.FunctionDeclaration:
         const fn = (node as ts.FunctionDeclaration);
         if (!fn.name) {
           return;
         }
-        if (/^(mx)?[A-Z]\w+$/.test(fn.name.text)) {
-          ctx.addFailureAtNode(node, `ES5 class - ${fn.name.text}`);
+        const fix = [
+          new Lint.Replacement(node.getStart(), node.getEnd() - node.getStart(), generateClass(fn)),
+        ];
+        if (isClassName(fn.name)) {
+          ctx.addFailureAtNode(node, `ES5 class - ${fn.name.getText()}`, fix);
         }
         break;
       case ts.SyntaxKind.ExpressionStatement:
@@ -46,7 +68,7 @@ function walk(ctx: Lint.WalkContext): void {
         }
         break;
       default:
-        console.debug('Unprocessed node kind: ', node.kind);
+        // console.debug('Unprocessed node kind: ', node.kind);
     }
   });
 }
